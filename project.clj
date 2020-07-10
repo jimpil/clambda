50436b0cf2e7caad07c644c7cc63ae31b7eb99da
Dimitrios Piliouras <jimpil1985@gmail.com>
2020-07-10 15:18:15 +0100

    update project.clj with :release-tasks

--- project.clj
+++ project.clj
@@ -8,4 +8,16 @@
                                   [org.clojure/data.csv "1.0.0"]
                                   [org.clojure/data.json "1.0.0"]]}}
   ;; exact match of the test dictionary
-  :jar-exclusions [#"ddict\.txt"])
+  :jar-exclusions [#"ddict\.txt"]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ;["vcs" "push"]
+                  ]
+  :deploy-repositories [["releases" :clojars]] ;; lein release :patch
+  :signing {:gpg-key "jimpil1985@gmail.com"})
